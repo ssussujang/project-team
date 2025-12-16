5ec2aadf7c53e573daf7b98372d58ef208bd96bd
raewon <kevinraewonkim@gmail.com>
2025-12-16 12:29:00 +0900

    controller 연결

--- receiptocr2/src/main/java/com/test/ocr/main/MainController.java
+++ receiptocr2/src/main/java/com/test/ocr/main/MainController.java
@@ -5,29 +5,31 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.test.ocr.DTO.ReceiptSaveDTO;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Controller
-@Getter
-@Setter
 public class MainController {
-	
-	@GetMapping("/")
-	
-	public String ocr(Model model) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (!model.containsAttribute("receiptForm")) {
-	        model.addAttribute("receiptForm", new ReceiptSaveDTO());
-	    }
-        // 로그인 여부 구분
-        boolean isLogin = !auth.getPrincipal().equals("anonymousUser");
+
+    @GetMapping("/")
+    public String ocr(Model model) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!model.containsAttribute("receiptForm")) {
+            model.addAttribute("receiptForm", new ReceiptSaveDTO());
+        }
+
+        boolean isLogin = auth != null && auth.isAuthenticated()
+                && !"anonymousUser".equals(auth.getPrincipal());
 
         model.addAttribute("isLogin", isLogin);
-		return "home";
-	}
+
+        return "home";
+    }
+
+    @GetMapping("/analysisPage")
+    public String analysisPage() {
+        return "user/analysisPage"; // templates/user/analysisPage.html
+    }
 }
