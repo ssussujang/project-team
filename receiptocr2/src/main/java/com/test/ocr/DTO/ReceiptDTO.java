c8d1c91c4c3a37629e6301d5b35e4c383018980b
raewon <kevinraewonkim@gmail.com>
2025-12-16 17:37:10 +0900

    DTO 수정 영수증 형식

--- receiptocr2/src/main/java/com/test/ocr/DTO/ReceiptDTO.java
+++ receiptocr2/src/main/java/com/test/ocr/DTO/ReceiptDTO.java
@@ -8,12 +8,19 @@ import lombok.Data;
 @Data
 public class ReceiptDTO {
 
-    private Long r_id;          // 영수증 ID (PK)
-    private String user_id;     // 어떤 사용자의 영수증인지
-    private String r_shop;      // 가게 이름
-    private String r_adrr;      // 주소
-    private LocalDate r_date;   // 날짜 
-    private int r_total;        // 총 금액
+	private Long r_no;			// 영수증 번호
+    private String r_u;     	// 어떤 사용자의 영수증인지
+    private String r_place;     // 가게 이름
+    private String r_price;		// 금액
+    private LocalDate r_date;   // 날짜
+    private String r_goods;    // r_goods : 상품명
+    private String category;  // category : 카테고리
+    private Gender gender;		//  enum : 성별
+
+    public enum Gender {
+        MALE, FEMALE
+    }
+    
     private List<ItemDTO> items;
     
     
